085994aad7387266d7de4c734118eb576eef23a2
Michael Iodice <miodice3@gmail.com>
2020-10-13 18:27:17 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,10 @@
+#Our job is to have the class somehow be able to keep track of all of the genres of all of the Books we create.
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -8,6 +12,11 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre    
   end
 
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,25 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    #binding.pry
+    if BRANDS == []
+      BRANDS << @brand
+    else
+      # BRANDS.detect do |b|
+      if !BRANDS.detect{|b| b== brand}
+          BRANDS << brand
+      end
+    end
   end
+#! operator with detect turns the return from nil or returning shoe brand to true or false. this allows it to be used as the condition for an if statement, SUPER HANDY
 
-  def cobble
+def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
